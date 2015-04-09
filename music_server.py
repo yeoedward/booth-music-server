29278ac883a27f4ca511a84e105f4bb8174d2cba
Eddy Yeo <edwardyeo@cmu.edu>
2015-04-09 19:14:01 -0400

    Done with basic functionality. Prev song not done.

--- music_server.py
+++ music_server.py
@@ -1,27 +1,29 @@
-# Adapted from https://pypi.python.org/pypi/jukebox-mpg123
+# Inspired by https://pypi.python.org/pypi/jukebox-mpg123
 
 from signal import SIGTSTP, SIGTERM, SIGABRT
-import daemon
-import daemon.pidfile
 import sys, os, subprocess
+import threading
 import time
 
-class MusicPlayer:
-    songs_idx = 0;
-    songs = [
-        'Lionhearted (Arty Radio Edit).mp3'
-    ];
-    proc = None
-    mpg123 = None
-    daemon_instance = None
-    pidFile = None
+class MusicPlayer(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+        # Extremely coarse-grained lock.
+        self.lock = threading.Lock()
+        self.songs_idx = 0;
+        self.songs = [
+            'Lionhearted (Arty Radio Edit).mp3'
+        ];
+        self.proc = None
+        self.mpg123 = None
+        self.running = True
 
     def getNextSong(self):
         nextSong = self.songs[self.songs_idx];
         self.songs_idx = (self.songs_idx + 1) % len(self.songs)
         return nextSong
 
-    def start(self):
+    def run(self):
         # Check if mpg123 is available
         fin, fout = os.popen4(["which", "mpg123"])
         self.mpg123 = fout.read().replace("\n", "")
@@ -29,46 +31,21 @@ class MusicPlayer:
             print "mpg123 is not installed"
             return
 
-        self.pidFile = os.path.dirname(os.path.abspath(__file__)) + \
-                "/daemon.pid"
-
-        print self.pidFile
-        if os.path.exists(self.pidFile):
-            print "Daemon already running, pid file exists"
-            return
-
-        pid = daemon.pidfile.TimeoutPIDLockFile(
-            self.pidFile,
-            10
-        )
-
         print "Starting mpg123 daemon..."
-        self.daemon_instance = daemon.DaemonContext(
-            uid=os.getuid(),
-            gid=os.getgid(),
-            pidfile=pid,
-            working_directory=os.getcwd(),
-            detach_process=True,
-            signal_map={
-                SIGTSTP: self.shutdown,
-                SIGABRT: self.skipSong
-            }
-        )
-
-        with self.daemon_instance:
-            self.play()
+        self.play()
 
     def stop(self):
-        if not os.path.exists(self.pidFile):
-            print "Daemon not running"
-            return
-
         print "Stopping daemon..."
-        pid = int(open(self.pidFile).read())
-        os.kill(pid, SIGTSTP)
+        self.lock.acquire()
+        self.running = False
+        if self.proc is not None:
+            os.kill(self.proc.pid, SIGTSTP)
+        self.lock.release()
 
     def play(self):
-        while True:
+        self.lock.acquire()
+        self.running = True
+        while self.running:
             if self.proc is None:
                 song = self.getNextSong()
 
@@ -78,22 +55,37 @@ class MusicPlayer:
 
                 print "Playing " + song
                 self.proc = subprocess.Popen(
-                    [self.mpg123, song]
+                    [self.mpg123, song],
+                    shell = False,
+                    stdout = subprocess.PIPE,
+                    stderr = subprocess.PIPE
                 )
             else:
                 if self.proc.poll() is not None:
                     self.proc = None
+            self.lock.release()
             time.sleep(0.5)
-
-    def shutdown(self):
-        print "Shutting down..."
-        if self.proc is not None:
-            os.kill(self.proc.pid, SIGTERM)
-
-        if self.daemon_instance is not None:
-            self.daemon_instance.close()
-        sys.exit(0)
+            self.lock.acquire()
 
     def skipSong(self):
+        self.lock.acquire()
         if self.proc is not None:
             os.kill(self.proc.pid, SIGTERM)
+        self.lock.release()
+
+musicPlayer = MusicPlayer()
+musicPlayer.start()
+
+while True:
+    command = raw_input('Command: ')
+    if command == 'quit':
+        musicPlayer.stop()
+        break
+    elif command == 'stop':
+        musicPlayer.stop()
+        # Threads can only be run once, so we create a new object.
+        musicPlayer = MusicPlayer()
+    elif command == 'play':
+        musicPlayer.start()
+    elif command == 'skip':
+        musicPlayer.skipSong()
